71cd32ca10c34762535209a51795753a1f9ed4e9
Brian Irving <brianirv@buffalo.edu>
2021-03-17 23:11:54 -0600

    Basic movement commit
    Initial Commit, was able to get some logic down. There's a storm trooper that is able to move around.

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace StormTrooperShooter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.txtAmmo = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.healthBar = new System.Windows.Forms.ProgressBar();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.trooper = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trooper)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtScore.Location = new System.Drawing.Point(12, 9);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(70, 25);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Text = "Kills: 0";
+            // 
+            // txtAmmo
+            // 
+            this.txtAmmo.AutoSize = true;
+            this.txtAmmo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtAmmo.Location = new System.Drawing.Point(12, 49);
+            this.txtAmmo.Name = "txtAmmo";
+            this.txtAmmo.Size = new System.Drawing.Size(91, 25);
+            this.txtAmmo.TabIndex = 2;
+            this.txtAmmo.Text = "Ammo: 0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label3.Location = new System.Drawing.Point(961, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Health";
+            // 
+            // healthBar
+            // 
+            this.healthBar.Location = new System.Drawing.Point(1049, 26);
+            this.healthBar.Name = "healthBar";
+            this.healthBar.Size = new System.Drawing.Size(184, 23);
+            this.healthBar.TabIndex = 4;
+            this.healthBar.Value = 100;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // trooper
+            // 
+            this.trooper.ErrorImage = null;
+            this.trooper.Image = global::StormTrooperShooter.Properties.Resources.troopright;
+            this.trooper.Location = new System.Drawing.Point(514, 367);
+            this.trooper.Name = "trooper";
+            this.trooper.Size = new System.Drawing.Size(148, 118);
+            this.trooper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.trooper.TabIndex = 5;
+            this.trooper.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(132)))), ((int)(((byte)(79)))));
+            this.ClientSize = new System.Drawing.Size(1245, 773);
+            this.Controls.Add(this.trooper);
+            this.Controls.Add(this.healthBar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAmmo);
+            this.Controls.Add(this.txtScore);
+            this.Name = "Form1";
+            this.Text = "Storm Trooper Shooter";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.trooper)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtAmmo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar healthBar;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.PictureBox trooper;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StormTrooperShooter
+{
+    public partial class Form1 : Form
+    {
+        bool goLeft;
+        bool goRight;
+        bool goUp;
+        bool goDown;
+        bool gameOver;
+        string facing = "right";
+        int trooperHealth = 100;
+        int speed = 10;
+        int ammo = 10;
+        int zombieSpeed = 3;
+        int score = 0;
+        Random ranNum = new Random();
+
+        List<PictureBox> jediHitBoxes = new List<PictureBox>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+     
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+            if (trooperHealth > 1)
+            {
+                healthBar.Value = trooperHealth;
+            }
+            else
+            {
+                gameOver = true;
+            }
+
+            txtAmmo.Text = "Ammo: " + ammo;
+            txtScore.Text="Kills:" + score;
+
+            if(goLeft==true && trooper.Left > 0)
+                //that method checks for space from the container to an object
+                //if there's space, 
+            {
+                trooper.Left -= speed;
+            }
+            if(goRight==true && trooper.Left + trooper.Width < this.ClientSize.Width)
+            {
+                trooper.Left += speed;
+            }
+            if(goUp==true && trooper.Top > 0)
+            {
+                trooper.Top -= speed;
+            }
+            if (goDown == true && trooper.Top + trooper.Height < this.ClientSize.Height)
+            {
+                trooper.Top += speed;
+            }
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+                facing = "left";
+                trooper.Image = Properties.Resources.troopleft;
+
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+                facing = "right";
+                trooper.Image = Properties.Resources.troopright;
+
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+                facing = "up";
+                trooper.Image = Properties.Resources.troopup;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+                facing = "down";
+                trooper.Image = Properties.Resources.troopdown;
+            }
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+             
+
+
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+                
+
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+        
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+             
+            }
+
+            if(e.KeyCode == Keys.Space){
+
+                ShootLazer(facing);
+
+            }
+
+
+        }
+
+        private void ShootLazer(string direction)
+        {
+
+        }
+
+        private void CreateJedi()
+        {
+
+        }
+
+        private void RestartGame()
+        {
+
+        }
+    }
+}
